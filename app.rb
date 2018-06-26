d8ea85ea88cbc702f4882350d2ff343996946b51
srappaport <sarah.rappaport@greendot.org>
2018-06-26 17:13:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,7 +1,5 @@
 class IdeaBoxApp < Sinatra::Base
   get '/' do
-    "<h1>Hello, World!</h1><blockquote>I guess I always felt even
-    if the world came to an end, McDonald's would still be open.
-      <cite>Susan Beth Pfeffer</cite></blockquote>"
+    erb :index
   end
   end
